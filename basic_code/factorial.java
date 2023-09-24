2ece5b1fc917298cacfb01de1fa4b466a50a617e
Anjali Dang <144944540+anjalidang@users.noreply.github.com>
2023-09-24 20:58:36 +0530

    factorial.java

--- basic_code/factorial.java
+++ basic_code/factorial.java
@@ -16,13 +16,3 @@ public class factorial {
 	}
 
 }
-////class Factorial{  
-//	 public static void main(String args[]){  
-//	  int i,fact=1;  
-//	  int number=7; 
-//	  for(i=1;i<=number;i++){    
-//	      fact=fact*i;    
-//	  }    
-//	  System.out.println("Factorial of "+number+" is: "+fact);    
-//	 }  
-//	} 
