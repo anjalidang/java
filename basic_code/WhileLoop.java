2cd6d683582dfd49d404e0a90e78b3fb6123023b
Anjali Dang <144944540+anjalidang@users.noreply.github.com>
2023-09-29 23:03:05 +0530

    Add files via upload

---
+++ basic_code/WhileLoop.java
@@ -0,0 +1,35 @@
+package gitHub;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+		//loop for printing multple times.
+		 int x=0;
+		 while (x < 5) {
+		 x++;
+		 System.out.println("Anjali");
+		 
+		 //Write a program to calculate the sum of first 10 natural number.
+		
+ }
+		int a = 1;
+		int b = 0;
+		while (a <= 5) {
+			b = a + b;
+			a++;
+		}
+		System.out.println(b);
+		
+		//Write a program to find the factorial value of any number entered through the keyboard.
+		int fact=1;
+		int num =1;
+		while(fact<=5) {
+			num=fact*num;
+					fact++;
+			
+		}
+		System.out.println(num);
+	}
+
+}
